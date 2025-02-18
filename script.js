3167992b4eb02d0915afaf3629bc0ebe8e562f91
Ujjwal9329 <ujjwalgujral8@gmail.com>
2025-02-19 01:22:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,58 @@
-//your code here
 document.addEventListener("DOMContentLoaded", function () {
     let draggedElement = null;
 
-    document.querySelectorAll(".draggable").forEach((div) => {
+    document.querySelectorAll(".image").forEach((div, index) => {
+        // Assigning a background image dynamically if missing
+        const bgImages = [
+            "url('https://picsum.photos/id/237/200/300')",
+            "url('https://picsum.photos/seed/picsum/200/300')",
+            "url('https://picsum.photos/200/300?grayscale')",
+            "url('https://picsum.photos/200/300/')",
+            "url('https://picsum.photos/200/300.jpg')",
+            "url('https://picsum.photos/id/102/200/300')"
+        ];
+        div.style.backgroundImage = bgImages[index];
+        div.style.height = "300px";
+        div.style.width = "200px";
+        div.style.margin = "10px";
+        div.style.display = "flex";
+        div.style.alignItems = "center";
+        div.style.justifyContent = "center";
+        div.style.color = "white";
+        div.style.fontWeight = "bold";
+
         div.addEventListener("dragstart", function (e) {
             draggedElement = this;
-            e.dataTransfer.setData("text/plain", this.id);
+            e.dataTransfer.setData("text/plain", this.innerText); // Store text
+            this.classList.add("selected");
         });
 
         div.addEventListener("dragover", function (e) {
             e.preventDefault();
         });
 
+        div.addEventListener("dragleave", function () {
+            this.classList.remove("selected");
+        });
+
         div.addEventListener("drop", function (e) {
             e.preventDefault();
             if (draggedElement !== this) {
+                // Swap inner text
+                let tempText = this.innerText;
+                this.innerText = draggedElement.innerText;
+                draggedElement.innerText = tempText;
+
                 // Swap background images
-                let temp = this.style.backgroundImage;
+                let tempBg = this.style.backgroundImage;
                 this.style.backgroundImage = draggedElement.style.backgroundImage;
-                draggedElement.style.backgroundImage = temp;
+                draggedElement.style.backgroundImage = tempBg;
             }
+            this.classList.remove("selected");
+        });
+
+        div.addEventListener("dragend", function () {
+            this.classList.remove("selected");
         });
     });
 });
