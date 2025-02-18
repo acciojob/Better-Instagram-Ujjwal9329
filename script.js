19510f77576d96e2b2549b0746e35754722a4cb0
Ujjwal9329 <ujjwalgujral8@gmail.com>
2025-02-19 01:19:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+document.addEventListener("DOMContentLoaded", function () {
+    let draggedElement = null;
+
+    document.querySelectorAll(".draggable").forEach((div) => {
+        div.addEventListener("dragstart", function (e) {
+            draggedElement = this;
+            e.dataTransfer.setData("text/plain", this.id);
+        });
+
+        div.addEventListener("dragover", function (e) {
+            e.preventDefault();
+        });
+
+        div.addEventListener("drop", function (e) {
+            e.preventDefault();
+            if (draggedElement !== this) {
+                // Swap background images
+                let temp = this.style.backgroundImage;
+                this.style.backgroundImage = draggedElement.style.backgroundImage;
+                draggedElement.style.backgroundImage = temp;
+            }
+        });
+    });
+});
